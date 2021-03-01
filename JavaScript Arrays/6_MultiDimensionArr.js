83d0f3e17afb2fffce2d0a7eb3d77f689a3af874
Aadi Manchekar <aadiprakash.manchekar2019@vitstudent.ac.in>
2021-03-01 16:30:29 +0530

    SECTION : 17

---
+++ JavaScript Arrays/6_MultiDimensionArr.js
@@ -0,0 +1,17 @@
+const gameBoard = [
+  ["X", "O", null],
+  [null, "O", "X"],
+  ["O", "X", null],
+];
+
+// so accessing the middle element
+console.log(gameBoard[1][1]);
+
+const colors = [
+  ["Red", "Crimson"],
+  ["Yellow", "Golden Rod"],
+  ["Blue", "Navy Blue"],
+  ["Purple", "Orchid"],
+];
+
+console.log(colors);
